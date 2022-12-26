7278049a6b79ce0312c818542ef4f2218021215b
SansonSebastiano <61165127+SansonSebastiano@users.noreply.github.com>
2022-12-26 17:23:50 +0100

    correzione database

--- database/eLusive.sql
+++ database/eLusive.sql
@@ -1,33 +1,39 @@
 
+CREATE TYPE role AS ENUM ('user','writer','admin');
+
 CREATE TABLE utente (
     id INT NOT NULL, 
     nome VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    ruolo ENUM("user","writer","admin") NOT NULL,
+    ruolo role NOT NULL,
     PRIMARY KEY(id)
-);µ
+);
+
+CREATE TYPE tag AS ENUM ('scoperta','new-entry','avvistamento','comunicazione','none');
 
 CREATE TABLE articolo (
     id INT NOT NULL,
     autore INT NOT NULL,
     titolo VARCHAR(255) NOT NULL,
-    data DATETIME NOT NULL,
+    data TIMESTAMP NOT NULL,
     descrizione VARCHAR(255) NOT NULL,
     contenuto VARCHAR(2000) NOT NULL,
-    image_path VARCHAR(1024)
-    tag ENUM("scoperta","new-entry","avvistamento","comunicazione","none") NOT NULL,
+    image_path VARCHAR(1024),
+    tag tag NOT NULL,
     featured BOOLEAN NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (autore) REFERENCES utente(id)
 );
 
+CREATE TYPE state AS ENUM ('scoperto','ipotizzato','avvistato');
+
 CREATE TABLE animale (
     nome VARCHAR(255) NOT NULL UNIQUE,
     descrizione VARCHAR(2000) NOT NULL,
-    status ENUM("scoperto","ipotizzato","avvistato") NOT NULL,
+    status state NOT NULL,
     data_scoperta DATE,
-    image_path VARCHAR(1024) NOT NULL
+    image_path VARCHAR(1024) NOT NULL,
     PRIMARY KEY (nome)
 );
 
@@ -36,7 +42,7 @@ CREATE TABLE articolo_animale (
     animale VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(articolo, animale),
     FOREIGN KEY(articolo) REFERENCES articolo(id),
-    FOREIGN KEY(animale) REFERENCES animale(id)
+    FOREIGN KEY(animale) REFERENCES animale(nome)
 );
 
 CREATE TABLE commento (
@@ -44,7 +50,7 @@ CREATE TABLE commento (
     articolo INT NOT NULL,
     utente INT NOT NULL,
     contenuto VARCHAR(255) NOT NULL,
-    data DATETIME NOT NULL,
+    data TIMESTAMP NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(articolo) REFERENCES articolo(id),
     FOREIGN KEY(utente) REFERENCES utente(id)
@@ -58,13 +64,15 @@ CREATE TABLE risposta (
     FOREIGN KEY(padre) REFERENCES commento(id)
 );
 
+CREATE TYPE vote AS ENUM ('YES','NO');
+
 CREATE TABLE voto (
     utente INT,
-    animale INT,
-    voto ENUM("YES","NO"),
+    animale VARCHAR(255) UNIQUE,
+    voto vote,
     PRIMARY KEY(utente,animale),
     FOREIGN KEY(utente) REFERENCES utente(id),
-    FOREIGN KEY(animale) REFERENCES animale(id)
+    FOREIGN KEY(animale) REFERENCES animale(nome)
 );
 
 
