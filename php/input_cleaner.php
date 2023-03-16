fa9ccc59be06f5b6c27c16d6c1853d9f32f89288
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-03-16 18:18:16 +0100

    Aggiunto input cleaner
    TO-DO:
    - Aggiungere controllo su input
    - testing

---
+++ php/input_cleaner.php
@@ -0,0 +1,9 @@
+<?php
+    function clearInput($value) {
+        $value = trim($value);          //trim() rimuove gli spazi bianchi (o altri caratteri) dall'inizio e dalla fine di una stringa
+        $value = strip_tags($value);    //strip_tags() rimuove le tag HTML e PHP da una stringa
+        $value = htmlentities($value); //htmlentities() converte i caratteri speciali in entità HTML
+        
+        return $value;
+    }
+?>
--- php/login_form.php
+++ php/login_form.php
@@ -1,39 +1,52 @@
 <?php
     // import the connection script
     require_once 'connection_database.php';
-    // get the data from the form
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    // create a query 
-    $query = "SELECT * FROM `utente` WHERE `nome` = '$username' AND `password` = '$password'";
-    // execute the query
-    $result = $mysqli->query($query);
-    // check if the query was executed successfully
-    if (!$result) {
-        echo "Error: " . $mysqli->error;
-        exit();
-    }
-    
-    echo '<br>';
-    echo '<br>';
-    echo 'Success: A proper query to MySQL eLusive was made.';
-    echo '<br>';
-    echo 'Number of rows: '.$result->num_rows;
-    echo '<br>';
-    echo '<br>';
+    // import input cleaner
+    include 'input_cleaner.php';
 
-     // iterate over the result set
-    // fetch each row as an associative array
-    while ($row = $result->fetch_assoc()) {
-        echo "USERNAME: " . $row["nome"] . "<br>";
-        echo "PASSWORD: " . $row["password"] . "<br>";
-        echo "EMAIL: " . $row["email"] . "<br>";
-        echo "RUOLO: " . $row["ruolo"] . "<br>";
-        echo "-----------------<br>";
-    }
+    if (isset($_POST['submit'])) {
+        // get the data from the form
+        $username = clearInput($_POST['username']);
+        if (empty($username)) {
+            echo "<li>Username is required<li>";
+        }  
 
-    // free the result set
-    $result->free();
-    // close the connection
-    $mysqli->close();
+        $password = clearInput($_POST['password']);
+        if (empty($password)) {
+            echo "<li>Password is required<li>";
+        }  
+
+        // create a query 
+        $query = "SELECT * FROM `utente` WHERE `nome` = '$username' AND `password` = '$password'";
+        // execute the query
+        $result = $mysqli->query($query);
+        // check if the query was executed successfully
+        if (!$result) {
+            echo "Error: " . $mysqli->error;
+            exit();
+        }
+        
+        echo '<br>';
+        echo '<br>';
+        echo 'Success: A proper query to MySQL eLusive was made.';
+        echo '<br>';
+        echo 'Number of rows: '.$result->num_rows;
+        echo '<br>';
+        echo '<br>';
+
+        // iterate over the result set
+        // fetch each row as an associative array
+        while ($row = $result->fetch_assoc()) {
+            echo "USERNAME: " . $row["nome"] . "<br>";
+            echo "PASSWORD: " . $row["password"] . "<br>";
+            echo "EMAIL: " . $row["email"] . "<br>";
+            echo "RUOLO: " . $row["ruolo"] . "<br>";
+            echo "-----------------<br>";
+        }
+
+        // free the result set
+        $result->free();
+        // close the connection
+        $mysqli->close();
+    } 
 ?>
