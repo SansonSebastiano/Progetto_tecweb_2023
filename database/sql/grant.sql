57c37c580154d4d9d97b8ec93727070ada927e53
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-03-22 09:58:23 +0100

    fix grant options

--- database/sql/grant.sql
+++ database/sql/grant.sql
@@ -7,16 +7,16 @@ GRANT SELECT ON `my_elusive`.* TO 'visitors'@'localhost';
 CREATE USER 'logged'@'localhost' IDENTIFIED BY 'logged';
 GRANT USAGE ON *.* TO 'visitors'@'localhost';
 GRANT SELECT ON `my_elusive`.* TO 'logged'@'localhost';
-GRANT INSERT ON `my_elusive`.commento, 'my_elusive'.voto TO 'logged'@'localhost';
+GRANT INSERT ON `my_elusive`.commento, `my_elusive`.voto TO 'logged'@'localhost';
 
 -- writers user type: also writer on specific tables
 CREATE USER 'writers'@'localhost' IDENTIFIED BY 'writers';
 GRANT USAGE ON *.* TO 'writers'@'localhost';
 GRANT SELECT ON `my_elusive`.* TO 'writers'@'localhost';
-GRANT INSERT ON `my_elusive`.commento, 'my_elusive'.voto, 'my_elusive'.articolo TO 'writers'@'localhost';
-GRANT DELETE ON 'my_elusive'.articolo TO 'writers'@'localhost' WHERE 'my_elusive'.articolo.autore = 'my_elusive'.utente.id;
+GRANT INSERT ON `my_elusive`.commento, `my_elusive`.voto, `my_elusive`.articolo TO 'writers'@'localhost';
+GRANT DELETE ON `my_elusive`.view_articolo_utente TO 'writers'@'localhost';
 
--- admins user type: also writer on specific tables
-CREATE USER 'admins'@'localhost' IDENTIFIED BY 'admins';
-GRANT USAGE ON *.* TO 'admins'@'localhost';
-GRANT ALL PRIVILEGES ON `my_elusive`.* TO 'admins'@'localhost';
+-- admin user type: also writer on specific tables
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT USAGE ON *.* TO 'admin'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `my_elusive`.* TO 'admin'@'localhost';
