36d29b57e150bb17b92c324225b01b90dd8d50cb
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-03-25 09:15:44 +0100

    fix commenti

--- database/sql/grant.sql
+++ database/sql/grant.sql
@@ -3,20 +3,20 @@ CREATE USER 'guest'@'localhost' IDENTIFIED BY 'guest';
 GRANT USAGE ON *.* TO 'guest'@'localhost';
 GRANT SELECT ON `my_elusive`.* TO 'guest'@'localhost';
 
--- logged user type: also writer on specific tables
+-- logged user type
 CREATE USER 'logged'@'localhost' IDENTIFIED BY 'logged';
 GRANT USAGE ON *.* TO 'logged'@'localhost';
 GRANT SELECT ON `my_elusive`.* TO 'logged'@'localhost';
 GRANT INSERT ON `my_elusive`.commento, `my_elusive`.voto TO 'logged'@'localhost';
 
--- writers user type: also writer on specific tables
+-- writers user type
 CREATE USER 'writer'@'localhost' IDENTIFIED BY 'writer';
 GRANT USAGE ON *.* TO 'writer'@'localhost';
 GRANT SELECT ON `my_elusive`.* TO 'writer'@'localhost';
 GRANT INSERT ON `my_elusive`.commento, `my_elusive`.voto, `my_elusive`.articolo TO 'writer'@'localhost';
 GRANT DELETE ON `my_elusive`.view_articolo_utente TO 'writer'@'localhost';
 
--- admin user type: also writer on specific tables
+-- admin user type
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 GRANT USAGE ON *.* TO 'admin'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `my_elusive`.* TO 'admin'@'localhost';
