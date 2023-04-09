7f772e577806027f5dce089976c432f789e125fd
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-04-09 17:03:32 +0200

    aggiunti template pagine rimanenti

---
+++ php/pages/article-list.php
@@ -0,0 +1,14 @@
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "check_conn.php");
+
+    session_start();
+    $_SESSION["prev_page"] = "./pages/article-list.php";
+
+    $page = file_get_contents(".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "html" . DIRECTORY_SEPARATOR . "article-list.html");
+
+    $page = str_replace("<user />", "Ciao, " . $_SESSION["username"], $page);
+    $page = str_replace("<userImg />", "<img src=\"../../images/icons/icon-user.png\" class = \"profile-pic\" alt = \"utente\"/>", $page);
+    $page = str_replace("<log_in_out />", $log_in_out, $page);
+    
+    echo $page;
+?>
