845deedede9bd8418235bdfce4892a1bb079a56d
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-04-15 12:54:52 +0200

    template article.php + fix rif

---
+++ php/pages/article.php
@@ -0,0 +1,15 @@
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "check-conn.php");
+
+    session_start();
+    $_SESSION["prev_page"] = "./pages/article.php";
+
+    $page = file_get_contents(".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "html" . DIRECTORY_SEPARATOR . "article.html");
+
+    $page = str_replace("<user />", "Ciao, " . $_SESSION["username"], $page);
+    $page = str_replace("<user-img />", "<img src=\"../../images/icons/icon-user.png\" class = \"profile-pic\" alt = \"utente\"/>", $page);
+    $page = str_replace("<log-in-out />", $log_in_out, $page);
+    $page = str_replace("<script-conn/>", $user, $page);
+    
+    echo $page;
+?>
