fbc1521d137a0896a72e14a783ff5d72feb60eac
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-04-15 12:56:59 +0200

    fix

--- php/pages/form-add-animal.php
+++ php/pages/form-add-animal.php
@@ -12,7 +12,7 @@
     $page = file_get_contents(".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "html" . DIRECTORY_SEPARATOR . "form-add-animal.html");
 
     $page = str_replace("<user />", "Ciao, " . $_SESSION["username"], $page);
-    $page = str_replace("<user-img />", "<img src=\"images/icons/icon-user.png\" class = \"profile-pic\" alt = \"utente\"/>", $page);
+    $page = str_replace("<user-img />", "<img src=\"../../images/icons/icon-user.png\" class = \"profile-pic\" alt = \"utente\"/>", $page);
     $page = str_replace("<log-in-out />", $log_in_out, $page);
     $page = str_replace("<script-conn/>", $user, $page);
     
