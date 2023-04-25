c687d8ec8028691c2b9755fa5f4664d42eea3605
Matteo Stocco <mstocco11@gmail.com>
2023-04-25 17:48:47 +0200

    small fix

--- php/add-article.php
+++ php/add-article.php
@@ -1,14 +1,14 @@
 <?php
     include_once "conn/writer-conn.php";
     session_start();
-
+    
     $titolo = $_POST['titolo'];
-    $sottotitolo = $_POST['sottotitolo'];
+    $sottotitolo = $_POST['Sottotitolo'];
     $tag = $_POST['tag'];
     $luogo = $_POST['luogo'];
     $data = $_POST['data_scrittura'];
     $testo = $_POST['testo'];
-    $autore = $_SESSION['username'];
+    $autore = $_SESSION['id'];
     $path = $_POST['hidden'];
 
     $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`alt`) VALUES ('$autore', '$titolo', '$data', '$luogo', '$sottotitolo', '$testo', '$path', '$tag', 0, 'testo')";
