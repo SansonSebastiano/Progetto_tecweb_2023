39a9eb8b48bd4fe1b32f8f7bfae99832e0ea76eb
Matteo Stocco <mstocco11@gmail.com>
2023-05-04 15:12:14 +0200

    minor fixes

--- php/add-article.php
+++ php/add-article.php
@@ -1,5 +1,5 @@
 <?php
-    include_once "conn/writer-conn.php";
+    include_once "conn/admin-conn.php";
     
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
@@ -13,17 +13,23 @@
     $testo = $_POST['testo'];
     $autore = $_SESSION['id'];
     $path = $_POST['hidden'];
+    $creatura = $_POST['creatura'];
 
-    $query = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`alt`) VALUES ('$autore', '$titolo', '$data', '$luogo', '$sottotitolo', '$testo', '$path', '$tag', 0, 'testo')";
+    $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`alt`) VALUES ('$autore', '$titolo', '$data', '$luogo', '$sottotitolo', '$testo', '$path', '$tag', 0, '$testo')";
     $queryResult = mysqli_query($mysqli, $sql);
-
-    //TODO: inserire articolo_animale
-    //$queryTwo = "INSERT INTO `articolo_animale` (`articolo`,`autore`) VALUES ('$articolo', '$autore')";
-
+    $id_query = "SELECT MAX(`id`) FROM `articolo`";
+    $queryResult = mysqli_query($mysqli,$id_query);
+    $data = mysqli_fetch_array($queryResult);
+    $id = $data[0];
     if ($queryResult) {
         // free the result set
         $queryResult->free();
+    }
+    //TODO: inserire articolo_animale
+    $sql = "INSERT INTO `articolo_animale` (`articolo`,`animale`) VALUES ('$id', '$creatura')";
+    $queryResult = mysqli_query($mysqli,$sql);
 
+    if ($queryResult) {
         header("Location: " . "." . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . "form-add-article.php ");
         exit();
     }
