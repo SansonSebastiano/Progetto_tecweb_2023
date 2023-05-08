ac8b7839843c76bb38c6361f2846849922e5a60d
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-05-08 15:46:32 +0200

    fix

--- php/suggestion.php
+++ php/suggestion.php
@@ -1,6 +1,16 @@
 <?php
-    include_once "conn/admin-conn.php";
-    session_start();
+    include ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config.php";
+    require ".." . DIRECTORY_SEPARATOR . "check-conn.php";
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if ($_SESSION['ruolo'] != 'admin' && $_SESSION['ruolo'] != 'writer') {
+        //echo "<script>alert('Spiacente! Non hai permessi di amministratore');</script>";
+        header("Location: " . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "index.php ");
+    }
+
     $sql = "SELECT `nome` FROM `animale`";
     $query = mysqli_query($mysqli, $sql);
     $json_array = array();
