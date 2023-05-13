06927b0024bc40d8529297152af8d63e058643af
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-05-13 18:03:06 +0200

    close #3

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     $page = str_replace("<log-in-out/>", $log_in_out, $page);
     $page = str_replace("<script-conn/>", $user, $page);
 
+    // LOGIN SECTION
     $admin_section = "<button class=\"btn-primary\" onclick=\"location.href='" . $admin_page_ref . "'\" tabindex='2'>Sezione Amministratore</button>";
 
     $writer_section = "<button class=\"btn-primary\" onclick=\"location.href='" . $faar_ref . "'\" tabindex='2'>Scrivi un nuovo articolo</button>";
@@ -32,6 +33,7 @@
         $page = str_replace("<writer-section/>", "", $page);
     }
 
+    // ASIDE CHART SECTION
     $homeChart = file_get_contents($modules_path . "home-chart.html");
 
     $query = 'SELECT nome, image_path, alt FROM view_animale_voto ORDER BY YES DESC LIMIT 5;';
@@ -52,6 +54,37 @@
     $queryResult->free();
 
     $page = str_replace("<home-chart-entries/>", $entries, $page);
+
+    // ARTICLES LIST SECTION
+    $article = file_get_contents($modules_path . "home-article-list.html");
+
+    $queryTwo = 'SELECT id, titolo, image_path, alt, tag, data FROM articolo ORDER BY data DESC LIMIT 6;';
+    $queryResultTwo = mysqli_query($mysqli, $queryTwo);
+
+    $entriesTwo = "";
+    while ($resultTwo = mysqli_fetch_assoc($queryResultTwo)) {
+
+        $entryTwo = $article;
+        $entryTwo = str_replace("<article-img/>", $resultTwo["image_path"], $entryTwo);
+        $entryTwo = str_replace("<article-img-alt/>", $resultTwo["alt"], $entryTwo);
+        $entryTwo = str_replace("<article-title/>", $resultTwo["titolo"], $entryTwo);
+        $entryTwo = str_replace("<article-tag/>", $resultTwo["tag"], $entryTwo);
+        $entryTwo = str_replace("<article-id/>", $resultTwo["id"], $entryTwo);
+
+        $entriesTwo .= $entryTwo;
+    }
+
+    $queryResultTwo->free();
+
+    $page = str_replace("<home-article-list-entries/>", $entriesTwo, $page);
+
+    // DATE SECTION
+    $queryThree = 'SELECT data FROM articolo ORDER BY data DESC LIMIT 1;';
+    $queryResultThree = mysqli_query($mysqli, $queryThree);
+
+    $resultThree = mysqli_fetch_assoc($queryResultThree);
+
+    $page = str_replace("<last-article-date/>", $resultThree["data"], $page);
 
     echo $page;
 ?>
