20a5a3e6640e3767dd3978200059b2e412dc7d2b
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-05-13 18:27:08 +0200

    start main article section

--- index.php
+++ index.php
@@ -33,58 +33,73 @@
         $page = str_replace("<writer-section/>", "", $page);
     }
 
+    // MAIN ARTICLES SECTION
+    $query = 'SELECT id, titolo, image_path, alt, tag, descrizione FROM articolo WHERE featured=1 ORDER BY data DESC LIMIT 5;';
+    $queryResult = mysqli_query($mysqli, $query);
+
+    while ($result = mysqli_fetch_assoc($queryResult)) {
+        $page = str_replace("<main-article-img/>", $result["image_path"], $page);
+        $page = str_replace("<main-img-alt/>", $result["alt"], $page);
+        $page = str_replace("<main-article-title/>", $result["titolo"], $page);
+        $page = str_replace("<main-article-tag/>", $result["tag"], $page);
+        $page = str_replace("<main-article-id/>", $result["id"], $page);
+        $page = str_replace("<main-article-subtitle/>", $result["descrizione"], $page);
+    }
+
     // ASIDE CHART SECTION
     $homeChart = file_get_contents($modules_path . "home-chart.html");
 
-    $query = 'SELECT nome, image_path, alt FROM view_animale_voto ORDER BY YES DESC LIMIT 5;';
-    $queryResult = mysqli_query($mysqli, $query);
-
-    $entries = "";
-
-    while ($result = mysqli_fetch_assoc($queryResult)) {
-
-        $entry = $homeChart;
-        $entry = str_replace("<chart-img/>", $result["image_path"], $entry);
-        $entry = str_replace("<chart-img-alt/>", $result["alt"], $entry);
-        $entry = str_replace("<animal-name/>", $result["nome"], $entry);
-
-        $entries .= $entry;
-    }
-
-    $queryResult->free();
-
-    $page = str_replace("<home-chart-entries/>", $entries, $page);
-
-    // ARTICLES LIST SECTION
-    $article = file_get_contents($modules_path . "home-article-list.html");
-
-    $queryTwo = 'SELECT id, titolo, image_path, alt, tag, data FROM articolo ORDER BY data DESC LIMIT 6;';
+    $queryTwo = 'SELECT nome, image_path, alt FROM view_animale_voto ORDER BY YES DESC LIMIT 5;';
     $queryResultTwo = mysqli_query($mysqli, $queryTwo);
 
     $entriesTwo = "";
+
     while ($resultTwo = mysqli_fetch_assoc($queryResultTwo)) {
 
-        $entryTwo = $article;
-        $entryTwo = str_replace("<article-img/>", $resultTwo["image_path"], $entryTwo);
-        $entryTwo = str_replace("<article-img-alt/>", $resultTwo["alt"], $entryTwo);
-        $entryTwo = str_replace("<article-title/>", $resultTwo["titolo"], $entryTwo);
-        $entryTwo = str_replace("<article-tag/>", $resultTwo["tag"], $entryTwo);
-        $entryTwo = str_replace("<article-id/>", $resultTwo["id"], $entryTwo);
+        $entryTwo = $homeChart;
+        $entryTwo = str_replace("<chart-img/>", $resultTwo["image_path"], $entryTwo);
+        $entryTwo = str_replace("<chart-img-alt/>", $resultTwo["alt"], $entryTwo);
+        $entryTwo = str_replace("<animal-name/>", $resultTwo["nome"], $entryTwo);
 
         $entriesTwo .= $entryTwo;
     }
 
     $queryResultTwo->free();
 
-    $page = str_replace("<home-article-list-entries/>", $entriesTwo, $page);
+    $page = str_replace("<home-chart-entries/>", $entriesTwo, $page);
 
-    // DATE SECTION
-    $queryThree = 'SELECT data FROM articolo ORDER BY data DESC LIMIT 1;';
+    // ARTICLES LIST SECTION
+    $article = file_get_contents($modules_path . "home-article-list.html");
+
+    $queryThree = 'SELECT id, titolo, image_path, alt, tag, data FROM articolo ORDER BY data DESC LIMIT 6;';
     $queryResultThree = mysqli_query($mysqli, $queryThree);
 
-    $resultThree = mysqli_fetch_assoc($queryResultThree);
+    $entriesThree = "";
+    while ($resultThree = mysqli_fetch_assoc($queryResultThree)) {
 
-    $page = str_replace("<last-article-date/>", $resultThree["data"], $page);
+        $entryThree = $article;
+        $entryThree = str_replace("<article-img/>", $resultThree["image_path"], $entryThree);
+        $entryThree = str_replace("<article-img-alt/>", $resultThree["alt"], $entryThree);
+        $entryThree = str_replace("<article-title/>", $resultThree["titolo"], $entryThree);
+        $entryThree = str_replace("<article-tag/>", $resultThree["tag"], $entryThree);
+        $entryThree = str_replace("<article-id/>", $resultThree["id"], $entryThree);
+
+        $entriesThree .= $entryThree;
+    }
+
+    $queryResultThree->free();
+
+    $page = str_replace("<home-article-list-entries/>", $entriesThree, $page);
+
+    // LAST UPDATE SECTION
+    $queryFour = 'SELECT data FROM articolo ORDER BY data DESC LIMIT 1;';
+    $queryResultFour = mysqli_query($mysqli, $queryFour);
+
+    $resultFour = mysqli_fetch_assoc($queryResultFour);
+
+    $page = str_replace("<last-article-date/>", $resultFour["data"], $page);
+
+    $queryResultFour->free();
 
     echo $page;
 ?>
