e70fd4e553771a52582ee698890aaa9f2ff5a231
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-05-17 14:28:30 +0200

    fix undefined 'username'

--- php/logout.php
+++ php/logout.php
@@ -3,8 +3,6 @@
         session_start();
     }
     session_destroy();
-    
-    //echo "<script>console.log('SESSION DESTROYED');</script>";
 
     header("Location: " . DIRECTORY_SEPARATOR . "index.php")
 ?>
--- php/pages/animal-list.php
+++ php/pages/animal-list.php
@@ -14,7 +14,7 @@
 
     $page = str_replace("<greet/>", "Ciao, ", $page);
     $page = str_replace("<user-img/>", $icon_user_ref, $page);
-    $page = str_replace("<user/>", $_SESSION["username"], $page);
+    $page = str_replace("<user/>", isset($_SESSION["username"]) ? $_SESSION["username"] : "", $page);
     $page = str_replace("<log-in-out/>", $log_in_out, $page);
     $page = str_replace("<script-conn/>", $logUserConn, $page);
 
