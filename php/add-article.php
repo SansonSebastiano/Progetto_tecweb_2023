b05c5b8a0cd5fa21e050fc0b2034509b0a373af5
Sebastiano Sanson <61165127+SansonSebastiano@users.noreply.github.com>
2023-05-28 15:08:38 +0200

    fix featured checkbox

--- php/add-article.php
+++ php/add-article.php
@@ -28,7 +28,9 @@
         $autore = $_SESSION['id'];
         $path = clearInput($_POST['image-path']);
         $creatura = clearInput(filter_input(INPUT_POST,"creatura",FILTER_SANITIZE_SPECIAL_CHARS));
-        $featured = $_POST['featured'];
+        $featured = isset($_POST['featured']) ? 1 : 0;
+
+        echo "<script>console.log('".$featured."')</script>
 
         $ok = true;
 
@@ -82,7 +84,7 @@
 
                 }else{   
                     $queryResult->free();
-                    $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`nome_animale`) VALUES ('$autore', '$titolo', NOW(), '$luogo', '$sottotitolo', '$testo', '$path', '$tag', $featured, '$creatura')";
+                    $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`nome_animale`, `alt`) VALUES ('$autore', '$titolo', NOW(), '$luogo', '$sottotitolo', '$testo', '$path', '$tag', '$featured', '$creatura', '$creatura')";
                     $queryResult = mysqli_query($mysqli,$sql);
 
                     $_SESSION["result"] = "<p class='success'>Articolo inserito con successo</p>";
@@ -91,7 +93,7 @@
             else
             {
                 $queryResult->free();
-                $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`nome_animale`) VALUES ('$autore', '$titolo', NOW(), '$luogo', '$sottotitolo', '$testo', '$path', '$tag', $featured, NULL)";
+                $sql = "INSERT INTO `articolo` (`autore`,`titolo`, `data`, `luogo`, `descrizione`,`contenuto`, `image_path`,`tag`,`featured`,`nome_animale`, `alt`) VALUES ('$autore', '$titolo', NOW(), '$luogo', '$sottotitolo', '$testo', '$path', '$tag', '$featured', NULL, 'no animal')";
                 $queryResult = mysqli_query($mysqli,$sql);
             }
 
@@ -102,5 +104,5 @@
         $mysqli->close();
 
         $SESSION["error-strings"] = $errorStrings;
-        header("Location: " . $_SESSION["prev_page"]);
+        //header("Location: " . $_SESSION["prev_page"]);
     }
