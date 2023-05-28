267c5aae14d2adec83b92ca4dbb09da8f12c0529
Matteo Stocco <mstocco11@gmail.com>
2023-05-28 18:12:18 +0200

    fix

--- php/suggestion.php
+++ php/suggestion.php
@@ -1,15 +1,13 @@
 <?php
-    include ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config.php";
+    include ".." . DIRECTORY_SEPARATOR . "config.php";
     include "db-conn.php";
 
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
-
     $sql = "SELECT `nome` FROM `animale`";
     $query = mysqli_query($mysqli, $sql);
     $json_array = array();
-
     while($data = mysqli_fetch_assoc($query)){
         $json_array[]= $data;
     }
