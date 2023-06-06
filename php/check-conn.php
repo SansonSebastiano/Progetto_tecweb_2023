66874551c1223285702363dd9d11b954f19085c4
Kikkoceccato <francesco.ceccato.2@studenti.unipd.it>
2023-06-06 17:16:32 +0200

    validato codice secondo standard XHTML5

--- php/check-conn.php
+++ php/check-conn.php
@@ -5,10 +5,10 @@
 
     // logout
     $logoutPath = $root_client_side . "php" . DIRECTORY_SEPARATOR . "logout.php";
-    $logout_ref = "<a href=" . $logoutPath . " tabindex='1'>" . $icon_logout_ref . "</a>";
+    $logout_ref = "<a href=\"" . $logoutPath . "\" tabindex='1'>" . $icon_logout_ref . "</a>";
     // login
     $loginPath = $root_client_side . "html" . DIRECTORY_SEPARATOR . "form-login.html";
-    $login_ref = "<a href=" . $loginPath . " tabindex='1'>Accedi</a>";
+    $login_ref = "<a href=\"" . $loginPath . "\" tabindex='1'>Accedi</a>";
 
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
