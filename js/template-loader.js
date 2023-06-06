bd6fd9ee5c25e3dfdb732a38d94acd2924557697
Nicola Ravagnan <nicolaravagnan4@gmail.com>
2023-06-06 21:27:49 +0200

    Separazione JS da HTML I

---
+++ js/template-loader.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#header").load("../../html/modules/header.html"); 
+    $("#footer").load("../../html/modules/footer.html"); 
+    $("#go-up-div").load("../../html/modules/go-up.html"); 
+});
--- php/check-conn.php
+++ php/check-conn.php
@@ -7,7 +7,7 @@
     $logoutPath = $root_client_side . "php" . DIRECTORY_SEPARATOR . "logout.php";
     $logout_ref = "<a href=" . $logoutPath . " tabindex='1'>" . $icon_logout_ref . "</a>";
     // login
-    $loginPath = $root_client_side . "html" . DIRECTORY_SEPARATOR . "form-login.html";
+    $loginPath = $root_client_side . "php" . DIRECTORY_SEPARATOR . "login.php";
     $login_ref = "<a href=" . $loginPath . " tabindex='1'>Accedi</a>";
 
     if (session_status() === PHP_SESSION_NONE) {
