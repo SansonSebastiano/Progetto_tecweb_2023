4825a8e6b7f75cee868f8f10f0c28fd66482f9f7
Kikkoceccato <francesco.ceccato.2@studenti.unipd.it>
2023-06-11 14:46:34 +0200

    minor fix

--- php/db-conn.php
+++ php/db-conn.php
@@ -1,6 +1,7 @@
 <?php
     $db_host = 'localhost';
     $db_user = 'fceccato';
+    $db_password = 'iiP5aiboo3mootho';
     $db_db = 'fceccato';
 
     $mysqli = @new mysqli($db_host, $db_user, $db_password, $db_db);
